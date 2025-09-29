c8a722a3328b70c0cb08f643d9bed505437ba02e
Marmotrolo <marmolomarnolo@gmail.com>
2025-09-29 09:01:34 +0200

    pruebas javascript

--- OPTATIVA/holamundo/js/myscript.js
+++ OPTATIVA/holamundo/js/myscript.js
@@ -51,7 +51,7 @@ console.log(typeof(testnull))
 
 var first_array = [];
 var secondarray= new Array(3);
-var third_array= new Array (3,5,7,8);
+var third_array= new Array (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
 var fifth_array= new Array (3,5,7,9);
 var sixth_array= [third_array, fifth_array];
 
@@ -68,3 +68,39 @@ console.log (sixth_array);
 sixth_array[1][1] =8;
 third_array[1] = 9;
 console.log (sixth_array)
+
+/*for( var i =0; third_array.length > i; i++ ){
+    console.log(third_array[i]);
+}*/
+
+/*for (var i=third_array.length-1; i>=0; i--){
+    console.log(third_array[i]);
+}*/
+
+/*var i =third_array.length-1;
+for(;i>=0;i--){
+    console.log (third_array[i])
+}*/
+
+third_array.forEach(function myFunction(item){
+    console.log(item)
+});
+
+let today =new Date();
+let first_october= new Date(2025,9,1);
+console.log(today);
+console.log(first_october)
+
+console.log(today.getDay());
+
+if (today>first_october){
+    console.log ("Today is after to first octuber")
+}
+else{
+    console.log("Today is before to first october")
+}
+
+
+function myFirstFunction(){
+    console.log ("ola")
+}
