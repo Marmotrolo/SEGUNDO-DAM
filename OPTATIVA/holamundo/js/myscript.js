6ba7222c9efed9439217b7d361fb273493d7e17e
Marmotrolo <marmolomarnolo@gmail.com>
2025-10-09 14:20:10 +0200

    Últimas cosas javascript

--- OPTATIVA/holamundo/js/myscript.js
+++ OPTATIVA/holamundo/js/myscript.js
@@ -130,18 +130,39 @@ console.log (second_div);
 
 console.log("#my_third_div")
  console.log(".pruebas")
-$("#btn").addEventListener("click",function(){
-    var input = document.createElement("ola");
-    input.setAttribute("type","email");
-    input.setAttribute("placeholder","E-mail");
+
+$("#btn").addEventListener("click", function(){
+    var input = document.createElement("input");
+    input.setAttribute("type", "email");
+    input.setAttribute("placeholder", "E-email");
     input.setAttribute("name", "emails[]");
     $("#form").appendChild(input);
-    myAlert("add new email input");
+    myAlert("Add new email input");
+        console.log($("body").children[1]);
+
+    
 });
 
+function bind_close(){
+    let elements =document.querySelectorAll(".close");
+    for(var i=elements.length-1; i>=0; i--){
+        let el= elements[i];
+        el.addEventListener("click", function(){
+this.parentNode.style.display="none";
+        });
+    }
+}
+
 function myAlert(msg){
+
     var div= document.createElement("div");
     div.classList.add("alert");
     div.innerHTML= msg;
+    var close= document.createElement("span");
+    close.style.float="right"
+    close.classList.add("close")
+    close.innerHTML ="X";
+    div.appendChild(close);
     $("body").insertBefore(div,$("body").firstChild);
+    bind_close();
 }
