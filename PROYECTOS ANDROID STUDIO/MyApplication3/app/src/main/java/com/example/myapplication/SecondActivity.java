bde2f7f97f5b62f10088d74149dd84176ccb8ea1
Marmotrolo <marmolomarnolo@gmail.com>
2025-11-06 14:32:36 +0100

    tutorial node npm

--- PROYECTOS ANDROID STUDIO/MyApplication3/app/src/main/java/com/example/myapplication/SecondActivity.java
+++ PROYECTOS ANDROID STUDIO/MyApplication3/app/src/main/java/com/example/myapplication/SecondActivity.java
@@ -14,7 +14,7 @@ public class SecondActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.second_activity);
+        setContentView(R.layout.pruebalinearlayout);
         Bundle b = getIntent().getExtras();
         Log.i("Datos", b.getString("Surname"));;
     }
